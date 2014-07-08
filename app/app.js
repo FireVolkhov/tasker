fd78dfde503dadffb145a13c4d5a740cef2ddcae
FireVolkhov <FireVolkhov@gmail.com>
2014-07-08 15:12:03 +0600

    Добавил оработку даты в json'е в объект

--- app/app.js
+++ app/app.js
@@ -16,5 +16,34 @@ angular.module('tasker', ['appController', 'taskList', 'top-menu', 'ui.router'])
 				templateUrl: "app/tasks-list/tasks-list.html",
 				controller: 'taskListController as tasksCtrl'
 			});
+
+		$httpProvider.defaults.transformResponse.push(function(responseData){
+			convertDateStringsToDates(responseData);
+			return responseData;
+		});
+
+		var regexIso8601 = /^(\d{4}|\+\d{6})(?:-(\d{2})(?:-(\d{2})(?:T(\d{2}):(\d{2})(:(\d{2})(\.(\d{1,}))?)?(Z|([\-+])(\d{2}):(\d{2}))?)?)?)?$/;
+
+		function convertDateStringsToDates(input) {
+			// Ignore things that aren't objects.
+			if (typeof input !== "object") return input;
+
+			for (var key in input) {
+				if (!input.hasOwnProperty(key)) continue;
+
+				var value = input[key];
+				var match;
+				// Check for string properties which look like dates.
+				if (typeof value === "string" && (match = value.match(regexIso8601))) {
+					var milliseconds = Date.parse(match[0]);
+					if (!isNaN(milliseconds)) {
+						input[key] = new Date(milliseconds);
+					}
+				} else if (typeof value === "object") {
+					// Recurse into object
+					convertDateStringsToDates(value);
+				}
+			}
+		}
 	}])
 ;
