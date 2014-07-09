ac091c272a0db03b2f1cfd9126c89545e9dd9282
FireVolkhov <FireVolkhov@gmail.com>
2014-07-09 11:49:01 +0600

    Доделал верстук директивы task

--- app/components/task/task-directive/task-directive.js
+++ app/components/task/task-directive/task-directive.js
@@ -39,22 +39,28 @@ angular.module('template/components/task/task-directive.html', []).run(['$templa
 	$templateCache.put('template/components/task/task-directive.html',
 			'<div class="taskDirective task-status-{{ task.$status }}" ng-hide="task.$hide" ng-form="form">' +
 			'	<div class="task-isFinished-container">' +
-			'		<input type="checkbox" data-ng-model="task.IsFinished" ng-disabled="task.IsFinished && !task.$edit" ng-click="finished()">' +
+			'		<i class="task-icon{{ task.IsFinished ? \'Check\' : \'UnCheck\' }}" ng-click="finished()"></i>' +
 			'	</div>' +
-			'	<div>' +
+			'	<div class="task-text-dueTime-buttons-container">' +
 			'		<div class="task-text">' +
 			'			<span ng-if="!task.$edit" >{{ task.Text }}</span>' +
 			'			<textarea ng-if="task.$edit" data-ng-model="task.Text" focus required></textarea>' +
 			'		</div>' +
-			'		<div class="task-dueTime">' +
-			'			<span ng-if="!task.$edit">{{ task.DueTime | date: \'dd.MM.yyyy H:mm\' }}</span>' +
-			'			<input ng-if="task.$edit" type="datetime-local" data-ng-model="task.DueTime" required>' +
-			'		</div>' +
-			'		<div class="task-buttons">' +
-			'			<button ng-if="!task.$edit" class="task-hideButton" ng-click="task.$hide = !task.$hide">Скрыть</button>' +
-			'			<button ng-if="!task.$edit" class="task-editButton" ng-click="task.$edit = true">Редактировать</button>' +
-			'			<button ng-if="task.$edit" class="task-cancelButton" ng-click="task.$edit = false">Отмена</button>' +
-			'			<button ng-if="task.$edit" class="task-saveButton" ng-click="task.$save()" ng-disabled="!task.$resolved || form.$invalid">Сохранить</button>' +
+			'		<div class="task-dueTime-buttons-container">' +
+			'			<div ng-if="!task.$edit">' +
+			'				<span class="task-dueTime">{{ task.DueTime | date: \'dd.MM.yyyy H:mm\' }}</span>' +
+			'				<div class="task-buttons">' +
+			'					<button class="task-hideButton" ng-click="task.$hide = !task.$hide">Скрыть</button>' +
+			'					<button class="task-editButton" ng-click="task.$edit = true">Редактировать</button>' +
+			'				</div>' +
+			'			</div>' +
+			'			<div ng-if="task.$edit">' +
+			'				<input type="datetime-local" data-ng-model="task.DueTime" required>' +
+			'				<div class="task-buttons">' +
+			'					<button class="task-cancelButton" ng-click="task.$edit = false">Отмена</button>' +
+			'					<button class="task-saveButton" ng-click="task.$save()" ng-disabled="!task.$resolved || form.$invalid">Сохранить</button>' +
+			'				</div>' +
+			'			</div>' +
 			'		</div>' +
 			'	</div>' +
 			'</div>'
