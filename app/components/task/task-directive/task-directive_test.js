344f0e6a6aded3c8882760e9b07ab3970395af4f
FireVolkhov <FireVolkhov@gmail.com>
2014-07-10 14:42:58 +0600

    Немго больше тестов

--- app/components/task/task-directive/task-directive_test.js
+++ app/components/task/task-directive/task-directive_test.js
@@ -8,6 +8,13 @@
 "use strict";
 
 describe('task-directive', function(){
+	var ptor = protractor.getInstance(),
+		task = {
+			Text: "Новый текст задачи",
+			DueTime: "25032014" + protractor.Key.RIGHT + "1700",
+			DueTimeText: "25.03.2014 17:00"
+		};
+
 	it('Изменение статуса задачи', function() {
 		element.all(by.repeater('task in tasksCtrl.tasks')).each(function(elem){
 			elem.findElement(by.css('[ng-click="finished()"]')).click();
@@ -15,6 +22,22 @@ describe('task-directive', function(){
 
 		element.all(by.repeater('task in tasksCtrl.tasks')).each(function(elem){
 			expect(elem.getAttribute('class')).toContain('task-status-finished');
+		});
+	});
+	it('Редактирование задачи', function(){
+		element.all(by.repeater('task in tasksCtrl.tasks')).each(function(elem){
+			ptor.actions().mouseMove(elem).perform();
+		    elem.findElement(by.css('.task-editButton')).click();
+			var textElem = elem.findElement(by.model('task.Text'));
+			var timeElem = elem.findElement(by.model('task.DueTime'));
+			textElem.clear();
+			textElem.sendKeys(task.Text);
+			timeElem.clear();
+			timeElem.sendKeys(task.DueTime);
+			elem.findElement(by.buttonText('Сохранить')).click();
+
+			expect(elem.findElement(by.model('task.Text')).getText()).toBe(task.Text);
+			expect(elem.findElement(by.model('task.DueTime')).getText()).toBe(task.DueTimeText);
 		});
 	});
 });
